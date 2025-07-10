0db5f240035ae7113438774ff3dd06b5e371a44c
Mohammad Sulaiman Khan <mohammadkhan0829@gmail.com>
2025-07-10 17:54:29 +0530

    authentication, login, logout routes

--- src/app.js
+++ src/app.js
@@ -21,8 +21,11 @@ app.use(cookieParser());
 import userRouter from "./routes/user.routes.js";
 
 // routes declaration
+
 // set base route to /api/v1/users
 app.use("/api/v1/users", userRouter);
+
+
 
 // ✅ Global error handler — must come after routes
 app.use((err, req, res, next) => {
@@ -36,15 +39,5 @@ app.use((err, req, res, next) => {
         message: err.message || "Internal Server Error",
     });
 });
-
-// app.use((err, req, res, next) => {
-//   const statusCode = err.statusCode || 500;
-
-//   res.status(statusCode).json({
-//     success: false,
-//     message: err.message || "Internal Server Error",
-//     ...(process.env.NODE_ENV === "development" && { stack: err.stack })
-//   });
-// });
 
 export { app };
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,9 +3,27 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt  from "jsonwebtoken";
+
+
+const generateAccessAndRefreshTokens = async(userId) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken;
+        await user.save({validateBeforSave: false});
+
+        return {accessToken, refreshToken};
+
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrond while generating refersh and access token")
+    }
+}
 
 const registerUser = asyncHandler(async (req, res) => {
-    
     // TODO:
     // get user details from frontend
     // validation - not empty
@@ -40,8 +58,12 @@ const registerUser = asyncHandler(async (req, res) => {
     // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
     let coverImageLocalPath;
-    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
-        coverImageLocalPath = req.files.coverImage[0].path
+    if (
+        req.files &&
+        Array.isArray(req.files.coverImage) &&
+        req.files.coverImage.length > 0
+    ) {
+        coverImageLocalPath = req.files.coverImage[0].path;
     }
 
     if (!avatarLocalPath) {
@@ -80,4 +102,76 @@ const registerUser = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, createdUser, "User registerd succesfully"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+    //TODO:
+    // check for  username or email
+    // check if user exists in db
+    // if user exists then check password
+    // give access token and refresh token
+    // send cookies
+    // send success
+
+    const { username, email, password } = req.body;
+
+    if (!(username || email)) {
+        throw new ApiError(400, "Username or Email is required");
+    }
+
+    const user = await User.findOne({
+        $or: [{ username }, { email }],
+    });
+
+    if(!user) {
+        throw new ApiError(404, "User does not exists!")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid user credentials")
+    }
+
+    const {refreshToken, accessToken} = await generateAccessAndRefreshTokens(user._id);
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken");
+    
+    const options = {
+        httpOnly: true,
+        secure: true,
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    }
+
+    return res.status(200).cookie("accessToken", accessToken, options).cookie("refreshToken", refreshToken, options).json(
+        new ApiResponse(200, {user: loggedInUser, accessToken, refreshToken}, "User Logged In Successfully!")
+    )
+});
+
+const logoutUser = asyncHandler(async(req, res) => {
+    //TODO: 
+    //find user by id (but how? it will come form auth middleware)
+    //remove refresh token and access token from cookies
+    // also remove refresh tokens in user db
+    // remove session
+    // logout success message
+
+    await User.findByIdAndUpdate(req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200).clearCookie("accessToken", options).clearCookie("refreshToken", options).json(new ApiResponse(200, {}, "User logged Out"));
+
+})
+
+export { registerUser, loginUser, logoutUser };
